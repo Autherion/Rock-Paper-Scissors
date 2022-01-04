4a0de737e3a318cfa2ff4e60d7bcd87232836d32
Autherion <walteraleighb@gmail.com>
2022-01-04 16:26:03 -0400

    Changed the game function

--- scripts.js
+++ scripts.js
@@ -13,15 +13,13 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection){
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
         return "It's a tie!";
-    } else if (playerSelection.toLowerCase() === "rock"  && computerSelection.toLowerCase() === "scissors"){
+    } else if ((playerSelection.toLowerCase() === "rock"  && computerSelection.toLowerCase() === "scissors") || 
+    (playerSelection.toLowerCase() === "scissors"  && computerSelection.toLowerCase() === "paper") ||
+    (playerSelection.toLowerCase() === "paper"  && computerSelection.toLowerCase() === "rock"))
+    {
         return "You win!";
-    } else if (playerSelection.toLowerCase() === "scissors"  && computerSelection.toLowerCase() === "paper"){
-        return "You win!";
-    } else if (playerSelection.toLowerCase() === "paper"  && computerSelection.toLowerCase() === "rock"){
-        return "You win!";
-    } else {
-        return "You lose";
     }
+    return "You lose";    
 }
 
 function game() {
