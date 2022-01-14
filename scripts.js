f894fd03bc8054242d5f50009b118c64009df458
Autherion <walteraleighb@gmail.com>
2022-01-14 13:52:03 -0400

    Changed the logic using DOM methods

--- scripts.js
+++ scripts.js
@@ -22,11 +22,29 @@ function playRound(playerSelection, computerSelection){
     return "You lose";    
 }
 
-function game() {
-    for (let i = 0; i < 5; i++){
-        let playerChoice = window.prompt("Pick rock, paper or scissors");
-        computerChoice = computerPlay();
-        console.log(playRound(playerChoice, computerChoice))
-    }
-}
+
+const button = document.querySelectorAll("button");
+const results = document.querySelector(".results");
+
+// Counter for both side
+let counterPlayer = 0;
+let counterComputer = 0;
+
+button.forEach((button) => {
+    
+    button.addEventListener('click', () => {
+        result = playRound(button.id, computerPlay());
+        if (result === "You win!") counterPlayer += 1;
+        else if (result === "You lose") counterComputer += 1;
+        if (counterPlayer === 5 || counterComputer === 5) {
+            counterPlayer = 0;
+            counterComputer = 0;
+            results.textContent = result;
+        }
+        else {  
+            results.textContent = `|| ${result}  || Player Score: ${counterPlayer}, Computer Score: ${counterComputer}`; 
+        }    
+    });
+});
+
 
